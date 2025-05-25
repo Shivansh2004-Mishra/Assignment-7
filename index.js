09376804be0496134f7e31fb73e506bd2b3082c1
Shivansh2004-Mishra <shivanshmishra0029@gmail.com>
2025-05-25 23:33:46 +0530

    Refactor server configuration to use dynamic port and improve dotenv loading

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const todoRoutes = require('./routes/todoRoutes');
+require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-
+const PORT = process.env.PORT || 3000;
 app.use('/', todoRoutes);
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
